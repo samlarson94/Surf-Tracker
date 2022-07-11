034808ba65d2d5141fe4fcf15456d1477fc99f56
samlarson94 <samlarson94@gmail.com>
2022-07-11 12:58:15 -0500

    Starting Script, adding OpenWeather API key to weather object.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create Obect to Store Weather Information needed to utilize API
+let weather = {
+    "apiKey": "2e29086a1bb8840c610e0d65481f5462",
+
+}
+
+// Convert City to Lat/Long via Direct GeoCode = https://openweathermap.org/api/geocoding-api
+// One Call = https://openweathermap.org/api/one-call-3
